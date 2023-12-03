fe1f00ca523847da51850cf0cc217abf21db5ad7
HabibaMohamed07 <Habiba2003520@miuegypt.edu.eg>
2023-12-03 22:00:19 +0200

    Functions

---
+++ Functions.py
@@ -0,0 +1,128 @@
+from Arrays import initial_perm, exp_d, per, sbox, final_perm
+def hex2bin(s):
+    mp = {
+        '0': "0000", '1': "0001", '2': "0010", '3': "0011",
+        '4': "0100", '5': "0101", '6': "0110", '7': "0111",
+        '8': "1000", '9': "1001", 'A': "1010", 'B': "1011",
+        'C': "1100", 'D': "1101", 'E': "1110", 'F': "1111"
+    }
+    return ''.join(mp[c] for c in s)
+
+
+def bin2hex(s):
+    mp = {
+        "0000": '0', "0001": '1', "0010": '2', "0011": '3',
+        "0100": '4', "0101": '5', "0110": '6', "0111": '7',
+        "1000": '8', "1001": '9', "1010": 'A', "1011": 'B',
+        "1100": 'C', "1101": 'D', "1110": 'E', "1111": 'F'
+    }
+
+    chunks = [s[i:i+4] for i in range(0, len(s), 4)]
+    return ''.join(mp[ch] for ch in chunks)
+
+
+
+
+def bin2dec(binary):
+
+	binary1 = binary
+	decimal, i, n = 0, 0, 0
+	while(binary != 0):
+		dec = binary % 10
+		decimal = decimal + dec * pow(2, i)
+		binary = binary//10
+		i += 1
+	return decimal
+
+# Decimal to binary conversion
+
+
+def dec2bin(num):
+    binary_result = bin(num).replace("0b", "")
+    if len(binary_result) % 4 != 0:
+        div = len(binary_result) // 4
+        div = int(div)
+        counter = (4 * (div + 1)) - len(binary_result)
+        for i in range(0, counter):
+            binary_result = '0' + binary_result
+    return binary_result
+
+
+
+def permute(k, arr, n):
+	permutation = ""
+	for i in range(0, n):
+		permutation = permutation + k[arr[i] - 1]
+	return permutation
+
+
+
+def shift_left(k, nth_shifts):
+	s = ""
+	for i in range(nth_shifts):
+		for j in range(1, len(k)):
+			s = s + k[j]
+		s = s + k[0]
+		k = s
+		s = ""
+	return k
+
+# calculating xow of two strings of binary number a and b
+
+
+def xor(a, b):
+	ans = ""
+	for i in range(len(a)):
+		if a[i] == b[i]:
+			ans = ans + "0"
+		else:
+			ans = ans + "1"
+	return ans
+
+
+
+def encrypt(pt, rkb, rk):
+	pt = hex2bin(pt)
+
+	# Initial Permutation
+	pt = permute(pt, initial_perm, 64)
+	print("After initial permutation", bin2hex(pt))
+
+	# Splitting
+	left = pt[0:32]
+	right = pt[32:64]
+	for i in range(0, 16):
+		# Expansion D-box: Expanding the 32 bits data into 48 bits
+		right_expanded = permute(right, exp_d, 48)
+
+		# XOR RoundKey[i] and right_expanded
+		xor_x = xor(right_expanded, rkb[i])
+
+		# S-boxex: substituting the value from s-box table by calculating row and column
+		sbox_str = ""
+		for j in range(0, 8):
+			row = bin2dec(int(xor_x[j * 6] + xor_x[j * 6 + 5]))
+			col = bin2dec(
+				int(xor_x[j * 6 + 1] + xor_x[j * 6 + 2] + xor_x[j * 6 + 3] + xor_x[j * 6 + 4]))
+			val = sbox[j][row][col]
+			sbox_str = sbox_str + dec2bin(val)
+
+		# Straight D-box: After substituting rearranging the bits
+		sbox_str = permute(sbox_str, per, 32)
+
+		# XOR left and sbox_str
+		result = xor(left, sbox_str)
+		left = result
+
+		# Swapper
+		if(i != 15):
+			left, right = right, left
+		print("Round ", i + 1, " ", bin2hex(left),
+			" ", bin2hex(right), " ", rk[i])
+
+	# Combination
+	combine = left + right
+
+	# Final permutation: final rearranging of bits to get cipher text
+	cipher_text = permute(combine, final_perm, 64)
+	return cipher_text
